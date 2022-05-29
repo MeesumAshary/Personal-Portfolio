a7245eccb00fdf90714eb946790f9c5717c614fa
MeesumAshary <meesumashary@gmail.com>
2022-05-29 16:23:22 -0700

    create style and functionallity of contact section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
-// Cached Elements
+
+/* ------Constants------ */
+// This array stores all the  objects that are made when user submits the form.
+const objectArray = [];
+
+
+/* ------Class------ */
+class User {
+	constructor(name, email, message) {
+		this.name = name;
+		this.email = email;
+		this.message = message;
+	}
+}
+
+/* ------Cached Elements------ */
+
+// The following code is for Modals in the interest section
 const poem1 = document.querySelector('#munif1');
 const poem2 = document.querySelector('#munif2');
 const poem3 = document.querySelector('#fl');
@@ -19,9 +36,11 @@ const modalCloseContainer3 = document.querySelector('.modal-close-container-3');
 const modalCloseContainer4 = document.querySelector('.modal-close-container-4');
 const modalCloseContainer5 = document.querySelector('.modal-close-container-5');
 const modalCloseContainer6 = document.querySelector('.modal-close-container-6');
-// const
 
-//  Event Listeners
+// The following code is for the form on the 'Contact' section
+
+/* ------Event Listeners------ */
+// The following code is for Modals in the interest section
 poem1.addEventListener('click', function () {
 	modalBg.classList.add('bg-active');
 });
@@ -59,3 +78,30 @@ modalCloseContainer5.addEventListener('click', function () {
 modalCloseContainer6.addEventListener('click', function () {
 	modalBg6.classList.remove('bg-active-6');
 });
+
+// The following code is for the form on the 'Contact' section
+
+const nameInput = document.querySelector('#your-name');
+const emailInput = document.querySelector('#your-email');
+const yourMessage = document.querySelector('#textarea-message');
+const btn = document.querySelector('button');
+console.log(btn);
+
+// The following is the event listener that records form data by creating objects and storing them in array, when the submit button is clicked.
+
+btn.addEventListener('click', createObjects);
+
+/*----- functions -----*/
+// The following function creates an object every time a user submits a form. All objects are then stored in an array called 'objectArray'. the function also clears all text from
+
+function createObjects(event) {
+	event.preventDefault();
+    objectArray.push(
+		new User(nameInput.value, emailInput.value, yourMessage.value)
+	);
+	console.log(objectArray);
+	nameInput.value = '';
+	emailInput.value = '';
+	yourMessage.value = '';
+	alert('Thank you for submitting your message!');
+}
